b660ec7b118e7807812ffb9c6ddf2b207f5eae1f
Nikita Batov <nikitabatov@gmail.com>
2013-09-24 13:55:14 +0400

    rewrite send data(i2c)

--- I2cConnection.cpp
+++ I2cConnection.cpp
@@ -30,6 +30,25 @@ static inline __s32 i2c_smbus_read_word_data(int file, __u8 command)
                return 0x0FFFF & data.word;
  }
 
+ static inline __s32 i2c_smbus_write_word_data(int file, __u8 command, 
+                                               __u16 value)
+ {
+        union i2c_smbus_data data;
+        data.word = value;
+        return i2c_smbus_access(file,I2C_SMBUS_WRITE,command,
+                                I2C_SMBUS_WORD_DATA, &data);
+}
+
+static inline __s32 i2c_smbus_write_byte_data(int file, __u8 command, 
+                                               __u8 value)
+ {
+        union i2c_smbus_data data;
+        data.byte = value;
+        return i2c_smbus_access(file,I2C_SMBUS_WRITE,command,
+                                I2C_SMBUS_BYTE_DATA, &data);
+ }
+
+
 I2cConnection::I2cConnection(QString DevPath, int DevId):
 i_DevPath(DevPath),
 i_DevId(DevId),
@@ -49,6 +68,13 @@ int I2cConnection::OpenConnection()
     		i_BusFd = -1;
     		return res;
   		}
+
+  if (ioctl(i_BusFd, I2C_SLAVE, i_DevId) != 0)
+  {
+    res = errno;
+    fprintf(stdout, "ioctl(%d, I2C_SLAVE, %d) failed: %d\n", i_BusFd, i_DevId, res);
+    return res;
+  }
   	return 0;
 }
 
@@ -67,28 +93,14 @@ int I2cConnection::CloseConnection()
 
 int I2cConnection::SendData(char* data, char size)
 {
-  // int res;
-  // if (ioctl(i_BusFd, I2C_SLAVE, i_DevId) != 0)
-  // {
-  //   res = errno;
-  //   fprintf(stdout, "ioctl(%d, I2C_SLAVE, %d) failed: %d\n", i_BusFd, i_DevId, res);
-  //   return res;
-  // }
-  // unsigned char cmd[size];
-  // for (int i = 0; i < size; ++i)
-  // {
-  // 	cmd[i] = data[i];
-  // }
-
-  // if ((res = write(i_BusFd, &cmd, sizeof(cmd)) != sizeof(cmd)))
-  // {
-  //   if (res >= 0)
-  //     res = E2BIG;
-  //   else
-  //     res = errno;
-  //   fprintf(stdout, "write(%d) failed: %d\n", i_BusFd, res);
-  //   return res;
-  // }
+  if (size == 2) 
+  {
+    i2c_smbus_write_byte_data(i_BusFd, data[0],data[1]);
+  }
+  else
+  {
+    i2c_smbus_write_byte_data(i_BusFd, data[0],data[1] | (data[2] << 8)]; 
+  }
 
 return 0;
 }
